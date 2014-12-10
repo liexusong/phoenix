ecb65b876c32b172364df5faf89cc64cbcc46655
unknown <liexusong@qq.com>
2014-12-11 01:13:43 +0800

    first commit

--- phoenix.c
+++ phoenix.c
@@ -35,32 +35,138 @@
 #define  PH_DEFAULT_PORT     8080
 #define  PH_DEFAULT_BACKLOG  1024
 #define  PH_DEFAULT_TIMEOUT  3000
+#define  PH_DEFAULT_BUFSIZE  256
+#define  PH_DEFAULT_INCSIZE  128
+#define  PH_MAX_HEADER_SIZE  2048
+
+
+typedef enum {
+    ph_header_s0,
+    ph_header_s1,
+    ph_header_s2,
+    ph_header_s3
+} ph_header_state;
 
 typedef struct {
-	int fd;
-	struct sockaddr_in addr;
+    int fd;
+    struct sockaddr_in addr;
+    char *buf;
+    char *pos;
+    char *last;
+    char *end;
+    ph_header_state state;
 } ph_connection_t;
 
 
 static char http200[] = "HTTP/1.0 200 OK\r\nContent-length: 15\r\n\r\nHello Liexusong";
 
+int ph_read_header(ph_connection_t *conn)
+{
+    while (conn->pos < conn->end) {
+
+        switch (conn->state) {
+        case ph_header_s0:
+            if (*conn->pos == '\r') {
+                conn->state = ph_header_s1;
+            } else {
+                conn->state = ph_header_s0;
+            }
+            break;
+
+        case ph_header_s1:
+            if (*conn->pos == '\n') {
+                conn->state = ph_header_s2;
+            } else {
+                conn->state = ph_header_s0;
+            }
+            break;
+
+        case ph_header_s2:
+            if (*conn->pos == '\r') {
+                conn->state = ph_header_s3;
+            } else {
+                conn->state = ph_header_s0;
+            }
+            break;
+
+        case ph_header_s3:
+            if (*conn->pos == '\n') {
+                conn->pos++; /* move the end of header */
+                return 0;
+            } else {
+                conn->state = ph_header_s0;
+            }
+            break;
+        }
+
+        conn->pos++;
+    }
+
+    return -1;
+}
+
 void ph_service_handler(void *arg)
 {
-	ph_connection_t *conn = arg;
-	char buf[1024];
-	int bytes;
+    ph_connection_t *conn = arg;
+    int bytes;
 
-	bytes = lthread_recv(conn->fd, buf, 1024, 0, PH_DEFAULT_TIMEOUT);
-	if (bytes == -2) { /* timeout */
-		close(conn->fd);
-		free(conn);
-		return;
-	}
+    conn->buf = malloc(PH_DEFAULT_BUFSIZE);
+    if (!conn->buf) {
+        close(conn->fd);
+        free(conn);
+        return;
+    }
 
-	lthread_send(conn->fd, http200, sizeof(http200) - 1, 0);
+    conn->pos = conn->buf;
+    conn->last = conn->buf;
+    conn->end = conn->buf + PH_DEFAULT_BUFSIZE;
 
-	close(conn->fd);
-	free(conn);
+    for ( ;; ) {
+
+        if (conn->end - conn->last <= 0) { /* resize read buffer */
+            if (conn->end - conn->buf >= PH_MAX_HEADER_SIZE) {
+                goto out;
+            }
+
+            int   size = (conn->end - conn->buf) + PH_DEFAULT_INCSIZE;
+            char *temp = realloc(conn->buf, size);
+
+            if (temp) {
+                int pos = conn->pos - conn->buf;
+                int last = conn->last - conn->buf;
+
+                conn->buf = temp;
+                conn->pos = conn->buf + pos;
+                conn->last = conn->buf + last;
+                conn->end = conn->buf + size;
+
+            } else {
+                goto out;
+            }
+        }
+
+        bytes = lthread_recv(conn->fd, conn->last,
+            conn->end - conn->last, 0, PH_DEFAULT_TIMEOUT);
+        if (bytes == -2) { /* timeout */
+            goto out;
+        }
+
+        conn->last += bytes;
+
+        if (!ph_read_header(conn)) { /* read header finish */
+            break;
+        }
+    }
+
+    fprintf(stderr, "Header length: %d, %s\n", conn->pos - conn->buf, conn->buf);
+
+    lthread_send(conn->fd, http200, sizeof(http200) - 1, 0);
+
+out:
+    close(conn->fd);
+    free(conn->buf);
+    free(conn);
+    return;
 }
 
 
@@ -69,32 +175,32 @@ void ph_service_handler(void *arg)
  */
 void ph_listen_handler(void *arg)
 {
-	int sfd, cfd;
-	int opt = 1;
-	struct sockaddr_in sin = {0};
-	struct sockaddr_in addr = {0};
-	socklen_t len = sizeof(addr);
-	int retval;
-	lthread_t *clt;
-	ph_connection_t *conn;
+    int sfd, cfd;
+    int opt = 1;
+    struct sockaddr_in sin = {0};
+    struct sockaddr_in addr = {0};
+    socklen_t len = sizeof(addr);
+    int retval;
+    lthread_t *clt;
+    ph_connection_t *conn;
 
-	DEFINE_LTHREAD; /* set lthread function name */
+    DEFINE_LTHREAD; /* set lthread function name */
 
-	sfd = lthread_socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    sfd = lthread_socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (sfd == -1) {
-    	fprintf(stderr, "Unable to create socket\n");
-    	return;
+        fprintf(stderr, "Unable to create socket\n");
+        return;
     }
 
     if (setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &opt,
         sizeof(int)) == -1)
     {
-    	close(sfd);
+        close(sfd);
         fprintf(stderr, "Unable to set SOREUSEADDR on socket\n");
         return;
     }
 
-	memset(&sin, 0, sizeof(sin));
+    memset(&sin, 0, sizeof(sin));
 
     sin.sin_family = PF_INET;
     sin.sin_addr.s_addr = INADDR_ANY;
@@ -102,16 +208,16 @@ void ph_listen_handler(void *arg)
 
     retval = bind(sfd, (struct sockaddr *)&sin, sizeof(sin));
     if (retval == -1) {
-    	close(sfd);
+        close(sfd);
         fprintf(stderr, "Unable to bind on port %d\n", PH_DEFAULT_PORT);
         return;
     }
 
     listen(sfd, PH_DEFAULT_BACKLOG);
 
-	for ( ;; ) {
+    for ( ;; ) {
 
-		cfd = lthread_accept(sfd, (struct sockaddr *)&addr, &len);
+        cfd = lthread_accept(sfd, (struct sockaddr *)&addr, &len);
         if (cfd == -1) {
             fprintf(stderr, "Unable to accept connection\n");
             continue;
@@ -119,25 +225,27 @@ void ph_listen_handler(void *arg)
 
         conn = malloc(sizeof(*conn));
         if (!conn) {
-        	close(cfd);
-        	continue;
+            close(cfd);
+            continue;
         }
 
         conn->addr = addr;
         conn->fd = cfd;
+        conn->state = ph_header_s0;
 
-        (void)lthread_create(&clt, 
-            (void *)ph_service_handler, (void *)conn);
-	}
+        (void)lthread_create(&clt, (void *)ph_service_handler, (void *)conn);
+    }
+
+    return;
 }
 
 
 int main(int argc, char *argv[])
 {
-	lthread_t *lt = NULL;
+    lthread_t *lt = NULL;
 
-	lthread_create(&lt, (void *)ph_listen_handler, NULL);
-	lthread_run();
+    lthread_create(&lt, (void *)ph_listen_handler, NULL);
+    lthread_run();
 
-	return 0;
+    return 0;
 }
